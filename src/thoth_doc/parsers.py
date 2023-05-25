adf6619c265db603b9c1e08c8ab7e1478f89fb8b
mariownyou <smj510black@gmail.com>
2023-05-25 15:15:57 +0200

    changes match to findall

--- src/thoth_doc/parsers.py
+++ src/thoth_doc/parsers.py
@@ -5,23 +5,26 @@ import re
 def env_var_parser(line):
     ''' Parses [$env.ENV_VAR_NAME] syntax '''
 
-    match = re.match(r'\[\$env\.(\w+)\]', line)
-    if match:
-        print(match.groups())
-        value = os.environ.get(match.group(1))
-        return line.replace(match.group(0), str(value))
+    matches = re.findall(r'(\[\$env\.(\w+)\])', line)
+    if matches:
+        for match, var_name in matches:
+            value = os.environ.get(var_name)
+            line = line.replace(match, str(value))
+        return line
     return None
 
 
 def django_settings_parser(line):
     ''' Parses [$settings.SETTINGS_VAR_NAME] syntax '''
 
-    match = re.match(r'\[\$settings\.(\w+)\]', line)
-    if match:
+    matches = re.findall(r'(\[\$settings\.(\w+)\])', line)
+    if matches:
         try:
             from django.conf import settings
         except ImportError:
             raise ImportError('Django is not installed')
-        value = getattr(settings, match.group(1))
-        return line.replace(match.group(0), str(value))
+        for match, setting_name in matches:
+            value = getattr(settings, setting_name)
+            line = line.replace(match, str(value))
+        return line
     return None

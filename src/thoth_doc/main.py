30f764fe8ac6ef56b4215029ebd18735d2d275bc
mariownyou <smj510black@gmail.com>
2023-05-30 16:02:19 +0200

    added MultiOutput class

--- src/thoth_doc/main.py
+++ src/thoth_doc/main.py
@@ -6,32 +6,21 @@ from .parsers import code_reference_parser
 class DocGenerator:
     parsers = []
     _default_parsers = [code_reference_parser]
-    output_dirs = []
 
     def __init__(self, docs_folder, compiled_docs_folder):
         self.docs_folder = docs_folder
         self.compiled_docs_folder = compiled_docs_folder
 
-    def _get_compiled_folder_path(self, path=None):
+    def _get_compiled_docs_path(self, path=None):
         cwd = os.getcwd()
         if path and path.startswith('/'):
             path = path[1:]
         return f'{cwd}/{self.compiled_docs_folder}/{path}' if path else f'{cwd}/{self.compiled_docs_folder}'
 
-    def _write_file(self, lines, filepath):
-        base = filepath.replace(self.docs_folder, '')
-        path = self._get_compiled_folder_path(base)
-
-        with open(path, 'w') as f:
-            f.write(lines)
-
-    def _parse_file(self, filepath):
+    def _compile_lines(self, markdown_lines):
         compiled_markdown = ''
 
-        with open(filepath) as f:
-            markdown = f.readlines()
-
-        for line in markdown:
+        for line in markdown_lines:
             for parser in self.parsers + self._default_parsers:
                 parsed = parser(line)
                 if parsed is not None:
@@ -41,26 +30,72 @@ class DocGenerator:
                 compiled_markdown += line
         return compiled_markdown
 
-    def _create_folder_structure(self):
-        compiled_path = self._get_compiled_folder_path()
-        os.makedirs(compiled_path, exist_ok=True)
+    def _create_folder_structure(self, cwd):
+        os.makedirs(cwd, exist_ok=True)
 
         for root, dirs, files in os.walk(self.docs_folder):
             for dir in dirs:
-                path = f'{compiled_path}/{root.replace(self.docs_folder, "")}/{dir}'
+                path = f'{cwd}/{root.replace(self.docs_folder, "")}/{dir}'
                 os.makedirs(path, exist_ok=True)
 
     def generate(self):
-        self._create_folder_structure()
+        self._create_folder_structure(self._get_compiled_docs_path())
 
         for root, dirs, files in os.walk(self.docs_folder):
             for file in files:
                 path = os.path.join(root, file)
                 if file.endswith('.md'):
-                    compiled_lines = self._parse_file(path)
-                    self._write_file(compiled_lines, path)
+                    with open(path) as f:
+                        lines = f.readlines()
+                    path = path.replace(self.docs_folder, '')
+                    path = self._get_compiled_docs_path(path)
+                    with open(path, 'w') as f:
+                        f.write(self._compile_lines(lines))
                 else:
-                    os.system(f'cp {path} {self._get_compiled_folder_path()}/{root.replace(self.docs_folder, "")}')
+                    os.system(f'cp {path} {self._get_compiled_docs_path()}/{root.replace(self.docs_folder, "")}')
+
+
+class MultiOutputDocGenerator(DocGenerator):
+    def __init__(self, docs_folder, compiled_docs_folder, output_folders):
+        super().__init__(docs_folder, compiled_docs_folder)
+        self.output_folders = output_folders
+
+    def _clean_lines(self, lines, output_folder):
+        ''' Removes block [only <dir>] ... [endonly] if dir != output_folder '''
+
+        clean_lines = []
+        skip = False
+        for line in lines:
+            if line.startswith('[only'):
+                if output_folder not in line:
+                    skip = True
+            elif line.startswith('[endonly]'):
+                skip = False
+            elif not skip:
+                clean_lines.append(line)
+        return clean_lines
+
+    def generate(self):
+        for output_folder in self.output_folders:
+            compiled_docs_path = f'{self._get_compiled_docs_path()}/{output_folder}'
+            self._create_folder_structure(compiled_docs_path)
+
+            for root, dirs, files in os.walk(self.docs_folder):
+                for file in files:
+                    path = os.path.join(root, file)
+                    if file.endswith('.md'):
+                        with open(path) as f:
+                            lines = f.readlines()
+
+                        path = path.replace(self.docs_folder, '')
+                        path = f'{output_folder}/{path}'
+                        path = self._get_compiled_docs_path(path)
+                        with open(path, 'w') as f:
+                            clean_lines = self._clean_lines(lines, output_folder)
+                            compiled_lines = self._compile_lines(clean_lines)
+                            f.write(self._compile_lines(compiled_lines))
+                    else:
+                        os.system(f'cp {path} {compiled_docs_path}/{root.replace(self.docs_folder, "")}')
 
 
 if __name__ == '__main__':
